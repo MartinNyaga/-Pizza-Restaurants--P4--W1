6230aab4d9814bcb1165b7c5f09c29b6fb470253
Martin Nyaga <martin.nyaga@student.moringaschool.com>
2023-09-25 23:00:30 +0300

    added swagger documentation

--- server/app.py
+++ server/app.py
@@ -3,10 +3,34 @@
 from flask import Flask, request, make_response,jsonify
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask_swagger_ui import get_swaggerui_blueprint
 
 from models import db, Restaurant, Pizza, RestaurantPizza
 
 app = Flask(__name__)
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/docs.json'  # Our API url (can of course be a local resource)
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+
+app.register_blueprint(swaggerui_blueprint)
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///meals.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
